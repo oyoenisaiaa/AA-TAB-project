8f6e93884a4e77fab226582809f9ef927a4af0be
Oyoenisai <oyoenisaiaa@gmail.com>
2024-07-05 09:35:19 -0400

    team update

--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -2,18 +2,55 @@ import React, { useEffect } from 'react';
 import { TiSocialLinkedin } from "react-icons/ti";
 
 // Import images
-// import senaImage from '../assets/team/Sena Dzokoto _ Ghana.jpg'; 
-// <img src="../assets/team/Sena Dzokoto _ Ghana.jpg" /> 
+import tinaImage from '../assets/team/Tina Ominiabohs- Nigeria_.jpg';
+import taiwoImage from '../assets/team/Taiwo Abdrasheed-Babatunde-Nigeria.jpg';
+import vanessaImage from '../assets/team/Vanessa Nelson-Dorglo.png';
+import temitopeImage from '../assets/team/Temitope Adebayo, Nigeria_.jpg';
+import senaImage from '../assets/team/Sena Dzokoto _ Ghana.jpg'; 
 import unwanaImage from '../assets/team/Unwanaonong Williams-Nigeria.jpg';
 import unomaImage from '../assets/team/Unoma Okoro_Nigeria.jpg';
 
-
+// import vanessanzImage from '../assets/team/Vanessa Nzurum- Nigeria.jpg';
+// import toluwaniImage from '../assets/team/Toluwani Oladapo_Nigeria.jpg';
+// import ariyoImage from '../assets/team/Ariyo Oluwadamilola. Nigeria_.png';
+// import elizabethImage from '../assets/team/Elizabeth Olaniyan - Nigeria_.jpg';
+// import ceciliaImage from '../assets/team/Cecilia Ajeleye - Nigeria.png';
+// import zinleImage from '../assets/team/Dladla Zinhle Fiona- Eswatini.jpg';
+// import ebosetaleImage from '../assets/team/Ebosetale Sarah, Nigeria_.jpg';
+// import ebeleImage from '../assets/team/Ebele Chukwu - Nigeria.jpg';
+// import ebunoluwaImage from '../assets/team/Ebunoluwa Oluwabukola Moronkola.png';
+// import ogunsebikanImage from '../assets/team/Janet ogunsebikan -Nigeria.jpg';
+// import celineImage from '../assets/team/Cecilia Ajeleye - Nigeria.png';
 
 
 const teamMembers = [
   {
+    name: "Tina OmIniabohs",
+    imgSrc: tinaImage, 
+    description: "Scrum Mastery(Chief Product Manager)",
+    linkedin: "http://linkedin.com/in/tina-ominiabohs-803a13264",
+  },
+  {
+    name: "Taiwo Hameedah Abdrasheed-Babatunde",
+    imgSrc: taiwoImage, 
+    description: "Scrum Mastery",
+    linkedin: "https://www.linkedin.com/in/taiwo-abdrasheed-babatunde-4a9735114",
+  },
+  {
+    name: "Vanessa Nelson-Dorglo",
+    imgSrc: vanessaImage, 
+    description: "Scrum Mastery",
+    linkedin: "https://www.linkedin.com/in/vanessa-nelson-dorglo-a17324169/",
+  },
+  {
+    name: "Vanessa Nelson-Dorglo",
+    imgSrc: temitopeImage, 
+    description: "Scrum Mastery",
+    linkedin: "https://www.linkedin.com/in/vanessa-nelson-dorglo-a17324169/",
+  },
+  {
     name: "Sena Dzokoto",
-    imgSrc: <img src="../assets/team/Sena Dzokoto _ Ghana.jpg" /> , 
+    imgSrc: senaImage,
     description: "Scrum mastery",
     linkedin: "https://www.linkedin.com/in/sena-dzokoto",
   },
@@ -29,7 +66,79 @@ const teamMembers = [
     description: "Product Management",
     linkedin: "https://www.linkedin.com/in/unomaokoro",
   },
+  
 ];
+
+// import  from '../assets/team/Vanessa Nelson-Dorglo.png';
+// import  from '../assets/team/Temitope Adebayo, Nigeria_.jpg';
+// import vanessanzImage from '../assets/team/Vanessa Nzurum- Nigeria.jpg';
+// 
+// import toluwaniImage from '../assets/team/Toluwani Oladapo_Nigeria.jpg';
+// import ariyoImage from '../assets/team/Ariyo Oluwadamilola. Nigeria_.png';
+// import elizabethImage from '../assets/team/Elizabeth Olaniyan - Nigeria_.jpg';
+// import ceciliaImage from '../assets/team/Cecilia Ajeleye - Nigeria.png';
+// import zinleImage from '../assets/team/Dladla Zinhle Fiona- Eswatini.jpg';
+// import ebosetaleImage from '../assets/team/Ebosetale Sarah, Nigeria_.jpg';
+// import ebeleImage from '../assets/team/Ebele Chukwu - Nigeria.jpg';
+// import ebunoluwaImage from '../assets/team/Ebunoluwa Oluwabukola Moronkola.png';
+// import ogunsebikanImage from '../assets/team/Janet ogunsebikan -Nigeria.jpg';
+// import celineImage from '../assets/team/Cecilia Ajeleye - Nigeria.png';
+
+// Olaniyan Elizabeth 
+// Product Design Track
+
+// Linked in:  www.linkedin.com/in/elizabeth-olaniyan-50418b271
+
+
+// Blessing Ozoemena 
+// LinkedIn url: https://www.linkedin.com/in/blessing-ozoemena-70106023b
+
+
+// Hephzibah Owoeye
+// Backend Development
+// https://www.linkedin.com/in/hephzibah-owoeye
+
+// Taiwo Rafiu 
+// Branding and marketing 
+// https://www.linkedin.com/in/taiwo-rafiu-211b69233?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app
+
+// Cecilia Ajeleye
+// http://www.linkedin.com/in/dacececi
+
+// Name: Otene Ojimaojo Eunice
+
+// Track: Data Science and Machine Learning
+
+// LinkedIn profile: https://www.linkedin.com/in/ojimaojo-eunice-otene-250b89185
+
+
+// Nzurum Chinonyerem Vanessa — 07/03/2024 1:35 PM
+// Nzurum Vanessa Chinonyerem 
+// Product management track
+
+// Link: https://www.linkedin.com/in/vanessa-nzurum-37964322b?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app
+
+
+// Nwobodo Rachael Chinwendu
+// Data science/Machine learning
+
+// https://www.linkedin.com/in/rachael-nwobodo-487a16102?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app
+
+// Full name: Ariyo Oluwadamilola Bukola
+// Track: Product Management Track.
+// LinkedIn Profile: https://www.linkedin.com/in/ariyo-oluwadamilola-b-2a956a248
+
+
+// https://www.linkedin.com/in/rashidat-tajudeen?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app
+//  I'm Rashidat Tajudeen from Data science and machine learning track 
+
+//  My name is Tina OmIniabohs
+// You can use my picture from the take a break hackathon
+// I’m the CPM.
+// This is my LinkedIn link
+// http://linkedin.com/in/tina-ominiabohs-803a13264
+
+
 
 const Team = () => {
   useEffect(() => {
